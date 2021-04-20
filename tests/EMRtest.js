4259aa9d3f8d1d49ec04d85372a5f90138d33bca
Ahmed-khoj <78299793+Ahmed-khoj@users.noreply.github.com>
2021-04-20 15:33:07 +0300

    Add files via upload

--- tests/EMRtest.js
+++ tests/EMRtest.js
@@ -7,14 +7,8 @@ contract("EMR", async function(accounts){
     it(
         "should add a record", async function(){
             let instance = await EMR.deployed()
-            await instance.addDocument("ahmed", "0x872a2c84021F9acbb586C30198a64e9C79723A94", "0x44Fca8abA3672D3aCeF0425378678B83064CCF26", "2020", "2021", "flu"), {from: accounts[0]};
+            await instance.addpatient(1, "ahmed", 22, "male","makkah", "flu"), {from: accounts[0]};
     })},
-
-    it(
-        "should get document", async function(){
-            let instance = await EMR.deployed()
-            await instance.getDocuments("0x872a2c84021F9acbb586C30198a64e9C79723A94");
-    }),
 
     it(
         "should give access", async function(){
@@ -27,3 +21,4 @@ contract("EMR", async function(accounts){
             let instance = await EMR.deployed()
             await instance.giveAccessToIns("0x44Fca8abA3672D3aCeF0425378678B83064CCF26");
     }));
+    
